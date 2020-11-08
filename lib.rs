240eda8844d55c68903ecf017f2b12a544bd4732
zhongzc <zhongzc_arch@outlook.com>
2020-11-08 15:59:19 +0800

    flush async logger when dropping global logger
    Signed-off-by: zhongzc <zhongzc_arch@outlook.com>

--- lib.rs
+++ lib.rs
@@ -11,11 +11,12 @@
 //! [`Logger`]: https://docs.rs/slog/2.4.1/slog/struct.Logger.html
 //! [slog-scope]: https://docs.rs/slog-scope
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use arc_swap::{ArcSwap, Guard};
 use lazy_static::lazy_static;
 use slog::{o, Logger};
+use slog_async::AsyncGuard;
 
 #[cfg(feature = "log")]
 mod log_redirect;
@@ -29,11 +30,29 @@ fn discard_logger() -> Logger {
 
 lazy_static! {
     static ref GLOBAL_LOGGER: ArcSwap<Logger> = ArcSwap::from(Arc::new(discard_logger()));
+    static ref GLOBAL_LOGGER_ASYNC_GUARD: Mutex<Option<AsyncGuard>> = Mutex::new(None);
 }
 
 /// Sets the global `Logger`.
-pub fn set_global(l: slog::Logger) {
+pub fn set_global(l: Logger) {
     GLOBAL_LOGGER.store(Arc::new(l));
+    GLOBAL_LOGGER_ASYNC_GUARD.lock().unwrap().take();
+}
+
+/// Sets the global `Logger` and [`AsyncGuard`].
+///
+/// If [`slog-async`] is used, the passed [`AsyncGuard`], created via [`AsyncBuilder::build_with_guard`],
+/// can guarantee logs will be flushed fully when clearing or switching the global `Logger`.
+///
+/// [`slog-async`]: https://crates.io/crates/slog-async
+/// [`AsyncGuard`]: https://docs.rs/slog-async/2.5.0/slog_async/struct.AsyncGuard.html\
+/// [`AsyncBuilder::build_with_guard`]: https://docs.rs/slog-async/2.5.0/slog_async/struct.AsyncBuilder.html#method.build_with_guard
+pub fn set_global_with_async_guard(logger: Logger, async_guard: AsyncGuard) {
+    GLOBAL_LOGGER.store(Arc::new(logger));
+    *GLOBAL_LOGGER_ASYNC_GUARD
+        .lock()
+        .unwrap()
+        .replace(async_guard);
 }
 
 /// Gets the global `Logger`.
@@ -52,6 +71,7 @@ pub fn borrow_global<'a>() -> Guard<'a, Arc<Logger>> {
 /// Clears the global `Logger` and discard future logging.
 pub fn clear_global() {
     GLOBAL_LOGGER.store(Arc::new(discard_logger()));
+    GLOBAL_LOGGER_ASYNC_GUARD.lock().unwrap().take();
 }
 
 /// Logs a critical level message using the global logger.
